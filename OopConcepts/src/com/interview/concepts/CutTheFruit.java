8d268cbc85f872e9a0abc737f9a5014f521e2a1c
Pavani Kadem <kadempavani7@gmail.com>
2020-08-21 00:36:24 +0530

    Code with all OOPS concepts

---
+++ OopConcepts/src/com/interview/concepts/CutTheFruit.java
@@ -0,0 +1,13 @@
+package com.interview.concepts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Abstraction
+public abstract class CutTheFruit {
+	public void twoSlices() {};
+	public void threeSlices() {
+		System.out.println("inside abstract");
+		
+	}	
+}
---
+++ OopConcepts/src/com/interview/concepts/Mango.java
@@ -0,0 +1,35 @@
+package com.interview.concepts;
+//Encapsulation
+public class Mango{
+	private String color;
+	private String taste;
+	private String name;
+
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getTaste() {
+		return taste;
+	}
+
+	public void setTaste(String taste) {
+		this.taste = taste;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
